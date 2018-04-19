e72c5511975251863bbdfd50f37b06dff3e57062
Slade Baumann <sladebaumann@users.noreply.github.com>
2018-04-18 23:10:32 -0500

    Update plex.sh

--- plex.sh
+++ plex.sh
@@ -8,8 +8,8 @@ wget -O - https://dev2day.de/pms/dev2day-pms.gpg.key | apt-key add -
 # add my PMS repo
 echo "deb https://dev2day.de/pms/ stretch main" >> /etc/apt/sources.list.d/pms.list
 # activate https
-apt-get install apt-transport-https
+apt-get install apt-transport-https -y
 # update the repos
 apt-get update
 # install PMS
-apt-get install plexmediaserver-installer
+apt-get install plexmediaserver-installer -y
